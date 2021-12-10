f1c6aad376be2a0a0bfc857566710202586e0954
sboucher <sboucher2011@gmail.com>
2021-12-10 10:09:49 -0500

    fixed issues with license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,10 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+function renderLicenseBadge(license) {
+    return 'https://img.shields.io/badge/licses-MIT-yellowgreen'
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -14,6 +18,8 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   const { username, ...title} = data;
   console.log("generateMarkdown");
+
+  renderLicenseBadge('MIT');
   return `# ${data.title}
   
 `;
